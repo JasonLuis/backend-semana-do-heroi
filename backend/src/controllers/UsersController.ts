ef1ff47d755f7a389be1b571a08a913902128bd1
JasonLuis <jason20luis@gmail.com>
2023-06-02 23:37:19 -0300

    chore: hero week part 2

--- backend/src/controllers/UsersController.ts
+++ backend/src/controllers/UsersController.ts
@@ -1,7 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { UsersServices } from "../services/UsersServices";
 
-
 class UsersController {
   private usersServices: UsersServices;
   constructor() {
@@ -28,24 +27,33 @@ class UsersController {
   async auth(request: Request, response: Response, next: NextFunction) {
     const { email, password } = request.body;
     try {
-        const result = await this.usersServices.auth(email, password);
-        return response.json(result);
+      const result = await this.usersServices.auth(email, password);
+      return response.json(result);
     } catch (error) {
-        next(error);
+      next(error);
+    }
+  }
+
+  async refresh(request: Request, response: Response, next: NextFunction) {
+    const { refresh_token } = request.body;
+    try {
+      const result = await this.usersServices.refresh(refresh_token);
+      return response.json(result);
+    } catch (error) {
+      next(error);
     }
   }
 
   async update(request: Request, response: Response, next: NextFunction) {
     const { name, oldPassword, newPassword, avatar_url } = request.body;
     const { user_id } = request;
-    try {    
-
+    try {
       const result = await this.usersServices.update({
         name,
         oldPassword,
         newPassword,
         avatar_url: request.file,
-        user_id
+        user_id,
       });
       return response.status(200).json(result);
     } catch (error) {
